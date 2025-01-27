a6819733c48813948c9dd5cafebf2648ab829e33
valeobli <a01285498@tec.mx>
2025-01-27 22:06:15 +0000

    fin

--- Herencia/main.cpp
+++ Herencia/main.cpp
@@ -11,13 +11,13 @@ using namespace std;
 int main() {
     list<Shape*> shapes;
 
-    // Add shapes to the list
+   
     shapes.push_back(new Circle(10, 20, 5.0));
     shapes.push_back(new Triangulo(15, 20, 5, 10));
     shapes.push_back(new Cuadrado(10, 20, 5));
     shapes.push_back(new Poligono(10, 20, 5, 15));
 
-    // Iterate and display the shapes
+   
     for (Shape* shape : shapes) {
         cout << shape->draw() << endl;
         cout << "Área: " << shape->calcularArea() << endl;
@@ -25,7 +25,7 @@ int main() {
         cout << "----------------------------" << endl;
     }
 
-    // Clean up memory
+   
     for (Shape* shape : shapes) {
         delete shape;
     }
